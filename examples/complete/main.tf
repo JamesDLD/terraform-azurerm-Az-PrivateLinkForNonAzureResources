191a031ef00953578ceb94bfc11ef0018a3bd337
DUMONT, James <jadumont@tf1.fr>
2022-11-14 22:06:34 +0100

    Ready to PR

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -17,73 +17,6 @@ provider "azurerm" {
   features {}
 }
 
-#Set authentication variables
-variable "tenant_id" {
-  description = "Azure tenant Id."
-}
-
-variable "subscription_id" {
-  description = "Azure subscription Id."
-}
-
-variable "client_id" {
-  description = "Azure service principal application Id."
-}
-
-variable "client_secret" {
-  description = "Azure service principal application Secret."
-}
-
-#Set resource variables
-
-variable "additional_tags" {
-  default = {
-    iac = "terraform"
-  }
-}
-
-variable "forwarding_rules" {
-  description = "Forwarding Rule to Endpoint (cf https://docs.microsoft.com/en-us/azure/data-factory/tutorial-managed-virtual-network-on-premise-sql-server?WT.mc_id=AZ-MVP-5003548&WT.mc_id=AZ-MVP-5003548#creating-forwarding-rule-to-endpoint)."
-  type        = any
-  default = {
-    "demo-static-website" = {
-      source_port         = "443"
-      destination_address = "demoprivatelinksftp.blob.core.windows.net"
-      destination_port    = "443"
-    }
-    "demo-sftp" = {
-      source_port         = "223"
-      destination_address = "demoprivatelinksftp.blob.core.windows.net"
-      destination_port    = "22"
-    }
-  }
-}
-
-variable "virtual_network" {
-  default = {
-    name          = "module-private-link-noprd-vnet4"
-    address_space = ["10.0.128.0/24", "198.18.2.0/24"]
-    subnets = {
-      privatelink = {
-        address_prefixes                              = ["10.0.128.0/28"]
-        private_link_service_network_policies_enabled = false
-      }
-
-      loadbalancer = {
-        address_prefixes = ["10.0.128.16/28"]
-      }
-
-      compute = {
-        address_prefixes = ["10.0.128.32/28"]
-      }
-
-      AzureBastionSubnet = {
-        address_prefixes = ["198.18.2.0/26"]
-      }
-    }
-  }
-}
-
 #Call native Terraform resources
 data "azurerm_resource_group" "rg" {
   name = "module-private-link-noprd-rg1"
@@ -138,8 +71,8 @@ resource "azurerm_subnet_nat_gateway_association" "Demo-Subnet-compute" {
 
 #Call module
 module "Az-PrivateLinkForNonAzureResources-Demo" {
-  #source = "git::https://github.com/JamesDLD/terraform-azurerm-Az-PrivateLinkForNonAzureResources.git//?ref=master"
-  source = "git::https://github.com/JamesDLD/terraform-azurerm-Az-PrivateLinkForNonAzureResources.git//?ref=release/0.2.0"
+  source = "git::https://github.com/JamesDLD/terraform-azurerm-Az-PrivateLinkForNonAzureResources.git//?ref=master"
+  #source = "git::https://github.com/JamesDLD/terraform-azurerm-Az-PrivateLinkForNonAzureResources.git//?ref=release/0.2.0"
   #source = "../../../"
   #source   = "JamesDLD/Az-PrivateLinkForNonAzureResources/azurerm"
   #version  = "0.2.0"
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,8 +1,70 @@
+#Set authentication variables
+variable "tenant_id" {
+  description = "Azure tenant Id."
+}
+
 variable "subscription_id" {
   description = "Azure subscription Id."
 }
 
-#Set variable
+variable "client_id" {
+  description = "Azure service principal application Id."
+}
+
+variable "client_secret" {
+  description = "Azure service principal application Secret."
+}
+
+#Set resource variables
+
+variable "additional_tags" {
+  default = {
+    iac = "terraform"
+  }
+}
+
+variable "forwarding_rules" {
+  description = "Forwarding Rule to Endpoint (cf https://docs.microsoft.com/en-us/azure/data-factory/tutorial-managed-virtual-network-on-premise-sql-server?WT.mc_id=AZ-MVP-5003548&WT.mc_id=AZ-MVP-5003548#creating-forwarding-rule-to-endpoint)."
+  type        = any
+  default = {
+    "demo-static-website" = {
+      source_port         = "443"
+      destination_address = "demoprivatelinksftp.blob.core.windows.net"
+      destination_port    = "443"
+    }
+    "demo-sftp" = {
+      source_port         = "223"
+      destination_address = "demoprivatelinksftp.blob.core.windows.net"
+      destination_port    = "22"
+    }
+  }
+}
+
+variable "virtual_network" {
+  default = {
+    name          = "module-private-link-noprd-vnet4"
+    address_space = ["10.0.128.0/24", "198.18.2.0/24"]
+    subnets = {
+      privatelink = {
+        address_prefixes                              = ["10.0.128.0/28"]
+        private_link_service_network_policies_enabled = false
+      }
+
+      loadbalancer = {
+        address_prefixes = ["10.0.128.16/28"]
+      }
+
+      compute = {
+        address_prefixes = ["10.0.128.32/28"]
+      }
+
+      AzureBastionSubnet = {
+        address_prefixes = ["198.18.2.0/26"]
+      }
+    }
+  }
+}
+
 variable "forwarding_rules" {
   description = "Forwarding Rule to Endpoint (cf https://docs.microsoft.com/en-us/azure/data-factory/tutorial-managed-virtual-network-on-premise-sql-server?WT.mc_id=AZ-MVP-5003548&WT.mc_id=AZ-MVP-5003548#creating-forwarding-rule-to-endpoint)."
   type        = any
