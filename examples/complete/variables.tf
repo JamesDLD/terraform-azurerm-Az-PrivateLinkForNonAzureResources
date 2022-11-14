dc2e0e920226e68b27ec36f83f54e4786baebf10
DUMONT, James <jadumont@tf1.fr>
2022-11-14 22:09:37 +0100

    Ready to PR

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -64,25 +64,3 @@ variable "virtual_network" {
     }
   }
 }
-
-variable "forwarding_rules" {
-  description = "Forwarding Rule to Endpoint (cf https://docs.microsoft.com/en-us/azure/data-factory/tutorial-managed-virtual-network-on-premise-sql-server?WT.mc_id=AZ-MVP-5003548&WT.mc_id=AZ-MVP-5003548#creating-forwarding-rule-to-endpoint)."
-  type        = any
-  default = {
-    "sql-demo1" = {
-      source_port         = "1433"
-      destination_address = "sql1.dld23.com"
-      destination_port    = "1433"
-    }
-    "sql-demo2" = {
-      source_port         = "1434"
-      destination_address = "sql2.dld23.com"
-      destination_port    = "1433"
-    }
-    "sftp-demo1" = {
-      source_port         = "221"
-      destination_address = "sftp.dld23.com"
-      destination_port    = "22"
-    }
-  }
-}
